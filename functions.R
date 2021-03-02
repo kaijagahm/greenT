ae503b53cbe8064698c28b5468f3f8e300c8fe66
Kaija Gahm (personal projects) <kaijagahmpersonalprojects@Kaijas-MacBook-Pro.local>
2021-03-02 17:17:20 -0500

    removed random color function

--- functions.R
+++ functions.R
@@ -1,10 +1,1 @@
 # Custom functions for the greenT app
-
-
-# Generate a random hex code ----------------------------------------------
-randomHex <- function(){
-  vec <- c(LETTERS, 0:9)
-  six <- sample(vec, 6, replace = T) %>% paste(collapse = "")
-  hex <- paste0("#", six)
-  return(hex)
-}
