5e4be7aef7059bac952d03c4220f849cbff5e293
Kaija Gahm (personal projects) <kaijagahmpersonalprojects@Kaijas-MacBook-Pro.local>
2021-03-02 18:18:35 -0500

    Made the color selectors collapsible

--- app.R
+++ app.R
@@ -8,10 +8,16 @@ library(forcats) # for dealing with factor recoding
 library(here) # for file paths
 library(randomcoloR) # for randomly-generated colors
 library(ggplot2)
+library(shinyBS)
 source(here("functions.R"))
 
-ui <- function(request){
+
+# UI ----------------------------------------------------------------------
+ui <- function(request){ # UI as a function to enable bookmarking
   fluidPage(
+    
+    # Change the font for the whole app -----------------------------------
+    ## white is already the default background color; just leaving this here in case we decide to change it later.
     tags$head(     
       tags$style(HTML("@import url('https://fonts.googleapis.com/css2?family=Baloo+2&display=swap');
       body {
@@ -22,63 +28,80 @@ ui <- function(request){
       }")
       )
     ),
-    titlePanel(title = "greenT"),
+    
+    # Title and subtitle, two different formats
+    titlePanel(div(HTML("<b>greenT  </b> <em><small>exploring grapheme-color synesthesia</em></small>"))),
+    
+    # Body
+    # Color pickers -------------------------------------------------------
     fluidRow(
-      column(width = 2,
-             colourInput("a", "A", value = randomColor()),
-             colourInput("g", "G", value = randomColor()),
-             colourInput("m", "M", value = randomColor()),
-             colourInput("s", "S", value = randomColor()),
-             colourInput("y", "Y", value = randomColor()),
-             colourInput("five", "5", value = randomColor())),
-      column(width = 2,
-             colourInput("b", "B", value = randomColor()),
-             colourInput("h", "H", value = randomColor()),
-             colourInput("n", "N", value = randomColor()),
-             colourInput("t", "T", value = randomColor()),
-             colourInput("z", "Z", value = randomColor()),
-             colourInput("six", "6", value = randomColor())),
-      column(width = 2,
-             colourInput("c", "C", value = randomColor()),
-             colourInput("i", "I", value = randomColor()),
-             colourInput("o", "O", value = randomColor()),
-             colourInput("u", "U", value = randomColor()),
-             colourInput("one", "1", value = randomColor()),
-             colourInput("seven", "7", value = randomColor())),
-      column(width = 2,
-             colourInput("d", "D", value = randomColor()),
-             colourInput("j", "J", value = randomColor()),
-             colourInput("p", "P", value = randomColor()),
-             colourInput("v", "V", value = randomColor()),
-             colourInput("two", "2", value = randomColor()),
-             colourInput("eight", "8", value = randomColor())),
-      column(width = 2,
-             colourInput("e", "E", value = randomColor()),
-             colourInput("k", "K", value = randomColor()),
-             colourInput("q", "Q", value = randomColor()),
-             colourInput("w", "W", value = randomColor()),
-             colourInput("three", "3", value = randomColor()),
-             colourInput("nine", "9", value = randomColor())),
-      column(width = 2,
-             colourInput("f", "F", value = randomColor()),
-             colourInput("l", "L", value = randomColor()),
-             colourInput("r", "R", value = randomColor()),
-             colourInput("x", "X", value = randomColor()),
-             colourInput("four", "4", value = randomColor()),
-             colourInput("zero", "0", value = randomColor()))),
+      bsCollapse(
+        id = "colorSelectors", open = "setColors",
+        bsCollapsePanel(
+          title = HTML("<em><small>Show/hide selectors</em></small>"),
+          value = "setColors",
+          column(width = 2,
+                 colourInput("a", "A", value = randomColor()),
+                 colourInput("g", "G", value = randomColor()),
+                 colourInput("m", "M", value = randomColor()),
+                 colourInput("s", "S", value = randomColor()),
+                 colourInput("y", "Y", value = randomColor()),
+                 colourInput("five", "5", value = randomColor())),
+          column(width = 2,
+                 colourInput("b", "B", value = randomColor()),
+                 colourInput("h", "H", value = randomColor()),
+                 colourInput("n", "N", value = randomColor()),
+                 colourInput("t", "T", value = randomColor()),
+                 colourInput("z", "Z", value = randomColor()),
+                 colourInput("six", "6", value = randomColor())),
+          column(width = 2,
+                 colourInput("c", "C", value = randomColor()),
+                 colourInput("i", "I", value = randomColor()),
+                 colourInput("o", "O", value = randomColor()),
+                 colourInput("u", "U", value = randomColor()),
+                 colourInput("one", "1", value = randomColor()),
+                 colourInput("seven", "7", value = randomColor())),
+          column(width = 2,
+                 colourInput("d", "D", value = randomColor()),
+                 colourInput("j", "J", value = randomColor()),
+                 colourInput("p", "P", value = randomColor()),
+                 colourInput("v", "V", value = randomColor()),
+                 colourInput("two", "2", value = randomColor()),
+                 colourInput("eight", "8", value = randomColor())),
+          column(width = 2,
+                 colourInput("e", "E", value = randomColor()),
+                 colourInput("k", "K", value = randomColor()),
+                 colourInput("q", "Q", value = randomColor()),
+                 colourInput("w", "W", value = randomColor()),
+                 colourInput("three", "3", value = randomColor()),
+                 colourInput("nine", "9", value = randomColor())),
+          column(width = 2,
+                 colourInput("f", "F", value = randomColor()),
+                 colourInput("l", "L", value = randomColor()),
+                 colourInput("r", "R", value = randomColor()),
+                 colourInput("x", "X", value = randomColor()),
+                 colourInput("four", "4", value = randomColor()),
+                 colourInput("zero", "0", value = randomColor()))
+        )
+      )
+    ),
     fluidRow(
       # Text input ---------------------------------------------------------
       column(width = 9,
-             textInput("displayText", "Text to display:", value = "test-123")
+             textInput("displayText", 
+                       "Text to display:", 
+                       value = "Type something")
       ),
       
       # Bookmark button ----------------------------------------------------
       column(width = 3,
-             br(),
+             br(), # just so the spacing aligns better with the text entry box
              bookmarkButton(label = "Save these colors",
                             icon = shiny::icon("heart-empty", lib = "glyphicon"))
       )
     ),
+    
+    # Output object -------------------------------------------------------
     fluidRow(
       plotOutput("colorBlocks")
     )
@@ -175,7 +198,6 @@ server <- function(input, output, session){
       geom_tile(aes(fill = hex))+
       scale_fill_identity()+
       theme_void()
-    
   })
   
 }
